226b474e0fd9c9a08fc93e3b2106b102cbe5d487
German Cruz <cruzgerman216@yahoo.com>
2020-03-19 17:24:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,9 @@
 class Triangle
   attr_accessor :side1, :side2, :side3
   # triangle code
+  def initialize(s1,s2,s3)
+    @side1, @side2, @side = s1,s2,s3
+  end
   def kind
     if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
       :equilateral
