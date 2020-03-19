1b6aae1133079d7c2236a9321b91f64757bfdaa2
German Cruz <cruzgerman216@yahoo.com>
2020-03-19 17:25:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,9 +5,11 @@ class Triangle
     @side1, @side2, @side = s1,s2,s3
   end
   def kind
+    test = nil
     if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
-      :equilateral
+      test = :equilateral
     end
+    test
   end
   class TriangleError < StandardError
     # triangle error code
