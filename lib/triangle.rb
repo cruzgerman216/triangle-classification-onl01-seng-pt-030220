5a5752088f92bc3df1b5a12a6e4ed364b976de94
German Cruz <cruzgerman216@yahoo.com>
2020-03-19 17:23:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,11 @@
 class Triangle
+  attr_accessor :side1, :side2, :side3
   # triangle code
-
+  def kind
+    if @side1 == @side2 && @side1 == @side3 && @side2 == @side3 
+      :equilateral
+    end
+  end
   class TriangleError < StandardError
     # triangle error code
   end
