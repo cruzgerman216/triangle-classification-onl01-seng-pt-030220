dc3dea943493843ca14d447cf35425f2af2d15fe
German Cruz <cruzgerman216@yahoo.com>
2020-03-19 17:34:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   def kind
     if @side1 == 0 || @side2 ==0 || @side3 ==-
       raise TriangleError
-    end 
+    end
     test = nil
     if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
       test = :equilateral
