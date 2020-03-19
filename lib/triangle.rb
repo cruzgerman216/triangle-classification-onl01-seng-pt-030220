51dd8352e0dfd6e156abc89fa5954fd069584b0d
German Cruz <cruzgerman216@yahoo.com>
2020-03-19 17:32:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,7 @@ class Triangle
     @side1, @side2, @side3= s1,s2,s3
   end
   def kind
-    
+
     test = nil
     if @side1 == @side2 && @side1 == @side3 && @side2 == @side3
       test = :equilateral
